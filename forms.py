e57d428d64d1456ffa713c31419c0ff98a56293c
tmouton1 <tamia.mouton1@gmail.com>
2023-02-23 15:48:19 -0500

    home forms work

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms import StringField, SubmitField, TextAreaField, IntegerField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length
 
 class TeamForm(FlaskForm):
-    team_name = StringField('team name', validators=[DataRequired(), Length(min=4, max=255)])
+    teamname = StringField('team name', validators=[DataRequired(), Length(min=4, max=255)])
     submit = SubmitField("submit")
 
 class ProjectForm(FlaskForm):
@@ -14,7 +14,12 @@ class ProjectForm(FlaskForm):
     submit = SubmitField("submit")
 
     def update_teams(self, teams):
-        self.team_selection.choices = [ (team.id, team.team_name) for team in teams]
+        self.team_selection.choices = [ (team.id, team.teamname) for team in teams]
+
+class DelForm(FlaskForm):
+
+    id = IntegerField("Id Number of Project to Remove: ")
+    submit = SubmitField("Remove Project")
 
 
 # class UserForm(FlaskForm):
--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, session, flash, request
-# from flask_wtf import FlaskForm
 from model import db, connect_to_db, User, Team, Project
-from forms import TeamForm, ProjectForm, LoginForm
+from forms import TeamForm, ProjectForm
 import jinja2
 
 app = Flask(__name__)
-app.secret_key = "keep this secret"
+app.secret_key = "mysecretkey"
 
 app.jinja_env.undefined = jinja2.StrictUndefined  
 
@@ -16,23 +15,25 @@ user_id = 1
 def home():
     team_form = TeamForm()
     project_form = ProjectForm()
-    project_form.update_teams(User.quer.get(user_id).teams)
+    project_form.update_teams(User.query.get(user_id).teams)
     return render_template("home.html", team_form = team_form, project_form = project_form)
+
 
 @app.route("/add-team", methods=["POST"])
 def add_team():
     team_form = TeamForm()
 
     if team_form.validate_on_submit():
-        team_name = team_form.team_name.data
-        new_team = Team(team_name, user_id)
+        teamname = team_form.teamname.data
+        new_team = Team(teamname, user_id)
         with app.app_context():
             db.session.add(new_team)
             db.session.commit()
+        print(team_form.teamname.data)
         return redirect(url_for("home"))
     else:
         return redirect(url_for("home"))
-
+    
 
 @app.route("/add-project", methods=["POST"])
 def add_project():
@@ -46,43 +47,16 @@ def add_project():
         team_id = project_form.team_selection.data
 
         new_project = Project(project_name, completed, team_id, description = description)
-        db.session.add(new_project)
-        db.session.commit()
+        with app.app_context():
+            db.session.add(new_project)
+            db.session.commit()
+
         return redirect(url_for("home"))
     else:
         return redirect(url_for("home"))
 
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-   """Log user into site."""
-   form = LoginForm(request.form)
-
-   if form.validate_on_submit():
-      username = form.username.data
-      password = form.password.data
-
-      user = user.get_by_username(username)
-
-      if not user or user['password'] != password:
-            flash("Invalid username or password")
-            return redirect('/login')
-
-      session["username"] = user['username']
-      flash("Logged in.")
-      return redirect("/")
-
-   return render_template("login.html", form=form)
-
-
-@app.route("/logout")
-def logout():
-
-   del session["username"]
-   flash("Logged out.")
-   return redirect("/login")
-
+# =================================
 if __name__ == "__main__":
     connect_to_db(app)
-    app.run(debug = True)
+    app.env = "development"
+    app.run(debug = True, port = 8000, host = "localhost")
